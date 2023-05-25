b74301457e5d7af104a89766d153a8418a2516d0
Khaled Abdou <khalid.elsehsah@gmail.com>
2023-05-25 18:09:56 +0800

    Profile Page & Dashboard

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,17 +1,97 @@
+import { useContext, useEffect, useReducer } from 'react';
+import { Store } from '../Store';
+import { getError } from '../utils';
+import axios from 'axios';
+import Loading from '../components/Loading';
+import Message from '../components/Message';
+import { Card, Col, Row } from 'react-bootstrap';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, loading: false, summary: action.payload };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 function DashboardPage() {
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'FETCH_REQUEST':
-        return { ...state, loading: true };
-      case 'FETCH_SUCCESS':
-        return { ...state, loading: false, summary: action.payload };
-      case 'FETCH_FAIL':
-        return { ...state, loading: false, error: action.payload };
-      default:
-        return state;
-    }
-  };
-  return <div>Dash</div>;
+  const [{ loading, summary, error }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+  });
+  const { state } = useContext(Store);
+  const { userInfo } = state;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get(
+          'http://localhost:5005/api/orders/summary',
+          {
+            headers: { authorization: `Bearer ${userInfo.token}` },
+          }
+        );
+        dispatch({ type: 'FETCH_SUCCESS', payload: data });
+      } catch (error) {
+        dispatch({ type: 'FETCH_FAIL', payload: getError(error) });
+      }
+    };
+    fetchData();
+  }, [userInfo]);
+  return (
+    <div>
+      <h1>Welcome to Dashboard</h1>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <>
+          <Row>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.users && summary.users[0]
+                      ? summary.users[0].numUsers
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Users</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.orders && summary.users[0]
+                      ? summary.orders[0].numOrders
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Orders</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>
+                    {summary.orders && summary.users[0]
+                      ? summary.orders[0].totalSales
+                      : 0}
+                  </Card.Title>
+                  <Card.Text>Total Sales</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default DashboardPage;
